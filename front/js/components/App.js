f60a4ec6554a33f664c5ef71e7d1dca5d1b9b27b
Peter Jabbour <pjabbour@urbanbound.com>
2019-02-23 17:19:36 -0600

    small changes

--- front/js/components/App.js
+++ front/js/components/App.js
@@ -4,12 +4,18 @@
 
 import React from 'react';
 
-import Timestamp from './Timestamp';
+import Timestamp from './timestamp';
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Hello World!</h1>
+      <h1>TODOs</h1>
+      <ul>
+        <li>React routes</li>
+        <li>Login form / flow</li>
+        <li>Redux</li>
+        <li>React-Bootstrap</li>
+      </ul>
       <Timestamp />
     </div>
   );
