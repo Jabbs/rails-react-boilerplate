19b165ee176e8a1588ada4cac80e852a144324b0
Peter Jabbour <pjabbour@urbanbound.com>
2019-02-23 15:39:48 -0600

    jwt and simple_command gems; authentication

--- Gemfile
+++ Gemfile
@@ -9,10 +9,12 @@ end
 gem 'bcrypt', '~> 3.1.7'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
+gem 'jwt'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'rails', '~> 5.2.2'
 gem 'sass-rails', '~> 5.0'
+gem 'simple_command'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 
---
+++ lib/json_web_token.rb
@@ -0,0 +1,16 @@
+# https://www.pluralsight.com/guides/token-based-authentication-with-ruby-on-rails-5-api
+class JsonWebToken
+  class << self # singleton class
+    def encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    end
+
+    def decode(token)
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      HashWithIndifferentAccess.new body
+    rescue
+      nil
+    end
+  end
+end
